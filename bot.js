512ed79181c7fe7bb3f134ce0f971aa59d1e2b67
BOUZOUANE <3673315@ppti-14-503-07.ufr-info-p6.jussieu.fr>
2017-03-22 11:20:11 +0100

    fix

--- bot.js
+++ bot.js
@@ -1,3 +1,23 @@
-module export = function (params)
-	this.bot=null;
-	this.
+const SlackBot = require('slackbots');
+module.exports = function(params) {
+	this.bot = null;
+	var self = this;
+	this.connect = function() {
+		self.bot = new SlackBot(params);
+		self.bot.on('start', this.onStart);
+		self.bot.on('message', this.onEvent);
+	}
+
+	self.onStart = function() {
+		self.bot.postMessageToUser('rimka', 'je suis là!!!!');
+
+		self.bot.getUser(self.bot.self.name).then(function(user) {
+			self.user = user;
+		});
+	}
+
+	self.onEvent = function(event) {
+		//reception d'un message
+		console.log(event);
+	}
+}
